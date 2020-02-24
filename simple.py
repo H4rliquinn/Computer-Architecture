48e8866be00e9dc49b7ab3220db6674d62387a94
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-24 13:33:13 -0500

    Print Num added

--- simple.py
+++ simple.py
@@ -2,13 +2,18 @@ import sys
 
 PRINT_BEEJ=1
 HALT=2
-
+PRINT_NUM=3
 
 memory=[
     PRINT_BEEJ,
+    PRINT_NUM,
+    1,
+    PRINT_NUM,
+    12,
     PRINT_BEEJ,
     PRINT_BEEJ,
-    PRINT_BEEJ,
+    PRINT_NUM,
+    37,
     PRINT_BEEJ,
     HALT
 ]
@@ -18,9 +23,14 @@ pc=0
 while True:
     command=memory[pc]
     if command==PRINT_BEEJ:
-        print("Nrrj!")
+        print("Beej!")
+    elif command==PRINT_NUM:
+        num=memory[pc+1]
+        print(num)
         pc+=1
     elif command==HALT:
         sys.exit(0)
     else:
         print("I did not understand that command")
+        sys.exit(1)
+    pc+=1
