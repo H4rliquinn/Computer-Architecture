536d06993aabdcebe5330ae97acb5a4f50fa3776
Jason Sonnichsen <Harliquinn@gmail.com>
2020-02-24 13:54:14 -0500

    Adding Registers

--- simple.py
+++ simple.py
@@ -3,20 +3,36 @@ import sys
 PRINT_BEEJ=1
 HALT=2
 PRINT_NUM=3
+SAVE=4 #Save to register
+PRINT_REGISTER=5 #Print val in registers
+ADD=6 #Add 2 registers
 
 memory=[
     PRINT_BEEJ,
-    PRINT_NUM,
-    1,
-    PRINT_NUM,
-    12,
-    PRINT_BEEJ,
-    PRINT_BEEJ,
-    PRINT_NUM,
-    37,
-    PRINT_BEEJ,
+    SAVE,
+    65,
+    2,
+    SAVE,
+    20,
+    3,
+    ADD,
+    2,
+    3,
+    PRINT_REGISTER,
+    2,
+    # PRINT_NUM,
+    # 1,
+    # PRINT_NUM,
+    # 12,
+    # PRINT_BEEJ,
+    # PRINT_BEEJ,
+    # PRINT_NUM,
+    # 37,
+    # PRINT_BEEJ,
     HALT
 ]
+
+register=[0]*8
 
 pc=0
 
@@ -30,6 +46,20 @@ while True:
         pc+=1
     elif command==HALT:
         sys.exit(0)
+    elif command==SAVE:
+        num=memory[pc+1]
+        reg = memory[pc+2]
+        register[reg]=num
+        pc+=2
+    elif command==PRINT_REGISTER:
+        reg=memory[pc+1]
+        print(register[reg])
+        pc+=1
+    elif command==ADD:
+        reg_a=memory[pc+1]
+        reg_b=memory[pc+2]
+        register[reg_a]+=register[reg_b]
+        pc+=2
     else:
         print("I did not understand that command")
         sys.exit(1)
